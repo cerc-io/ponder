4c37c8387c24f2d6ac741a0fdca45e519d88c93c
Nabarun <nabarun@deepstacksoft.com>
2023-09-14 15:23:51 +0530

    Pass and return cursor for pagination in postgres event-store

--- packages/core/src/event-store/postgres/store.ts
+++ packages/core/src/event-store/postgres/store.ts
@@ -19,7 +19,7 @@ import { intToBlob } from "@/utils/encode";
 import { mergeIntervals } from "@/utils/intervals";
 import { range } from "@/utils/range";
 
-import type { EventStore } from "../store";
+import type { Cursor, EventStore } from "../store";
 import {
   type EventStoreTables,
   type InsertableBlock,
@@ -412,12 +412,12 @@ export class PostgresEventStore implements EventStore {
       : null;
   };
 
-  // TODO: Pass and return cursor for pagination
   async *getLogEvents({
     fromTimestamp,
     toTimestamp,
     filters = [],
     pageSize = 10_000,
+    cursor,
   }: {
     fromTimestamp: number;
     toTimestamp: number;
@@ -431,6 +431,7 @@ export class PostgresEventStore implements EventStore {
       includeEventSelectors?: Hex[];
     }[];
     pageSize: number;
+    cursor?: Cursor;
   }) {
     const baseQuery = this.db
       .with(
@@ -619,15 +620,6 @@ export class PostgresEventStore implements EventStore {
       count: Number(c.count),
     }));
 
-    let cursor:
-      | {
-          timestamp: Buffer;
-          chainId: number;
-          blockNumber: Buffer;
-          logIndex: number;
-        }
-      | undefined = undefined;
-
     while (true) {
       let query = includedLogsBaseQuery.limit(pageSize);
       if (cursor) {
@@ -780,6 +772,7 @@ export class PostgresEventStore implements EventStore {
         metadata: {
           pageEndsAtTimestamp,
           counts: eventCounts,
+          cursor,
         },
       };
 
