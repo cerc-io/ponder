2221107dafb48c2875c197ab47460e8e3db73d62
Nabarun Gogoi <95nikass@gmail.com>
2023-10-04 11:42:50 +0530

    Make sequential RPC method requests when Nitro payments is configured (#5)
    * Start realtime after historical sync serve if nitro payment configured
    
    * Use maxRpcRequestConcurrency in realtime sync service
    
    * Add comment for starting realtime sync service after historical sync completes

--- packages/core/src/realtime-sync/service.ts
+++ packages/core/src/realtime-sync/service.ts
@@ -404,7 +404,7 @@ export class RealtimeSyncService extends Emittery<RealtimeSyncEvents> {
       );
 
       // Fetch all missing blocks using a request concurrency limit of 10.
-      const limit = pLimit(10);
+      const limit = pLimit(this.network.maxRpcRequestConcurrency);
 
       const missingBlockRequests = missingBlockNumbers.map((number) => {
         return limit(async () => {
